b5d2001b1976e22077b1fdf0446aa2fbc249d570
Chris Dinh <dinh.thanhtoan@gmail.com>
2022-01-23 23:30:07 +1300

    Getting a simple Statiq.Web project up and running

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Statiq.App;
+using Statiq.Web;
+
+namespace AnIotOdyssey
+{
+   public class Program
+   {
+      public static async Task<int> Main(string[] args) =>
+        await Bootstrapper
+          .Factory
+          .CreateWeb(args)
+          .RunAsync();
+   }
+}
